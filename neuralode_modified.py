c411606ab117767891fa9b166dd91158b7c202e9
Ganesh Ravisankar <52727958+ganesh1102@users.noreply.github.com>
2023-06-14 11:20:09 -0500

    Error with `torch.Tensor` shapes: SVI inference

---
+++ neuralode_modified.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import itertools
+
+import torch
+from torch import nn
+from torch.func import vmap, jacrev
+
+import tqdm
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append('../..')
+
+from pyoptmat import ode, utility
+
+if torch.cuda.is_available():
+	dev = 'cuda:0'
+else:
+	dev = 'cpu'
+
+device = torch.device(dev)
+
+
+
+class NeuralODE(nn.Module):
+	def __init__(self, model, force, n_hidden, n_layers, n_inter):
+		super().__init__()
+		self.model = model
+		self.force = force
+		self.n_in = n_hidden + 2 
+		self.n_out = n_hidden + 1
+		self.n_inter = n_inter + 1
+		
+	def forward(self, t, y):
+		inp = torch.empty(y.shape[:-1] + (self.n_in), device = device)
+		inp[..., :-1] = y
+		inp[..., -1] = self.force(t)
+
+		return self.model(inp), vmap(vmap(jacrev(self.model)))(inp)[..., :-1]
+
+	def initial(self, nsamples):
+		return torch.zeros(nsamples, self.n_out, device = device)
+
+
+
